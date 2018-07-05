020e287fee2fb10ab0d91060a7e20ae8960c65dd
Pepe Valdivia <jvaldivia@softweb.pe>
2018-07-04 21:12:44 -0500

    Moviendo archivos php a src/

--- src/configs/settings.php
+++ src/configs/settings.php
@@ -5,7 +5,7 @@ return [
     'addContentLengthHeader' => false, // Allow the web server to send the content-length header
     // Renderer settings
     'renderer' => [
-      'template_path' => __DIR__ . '/../../templates/',
+      'template_path' => __DIR__ . '/../templates/',
     ],
     // Monolog settings
     'logger' => [
