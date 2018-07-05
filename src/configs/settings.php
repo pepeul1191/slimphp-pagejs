9c8014551e96a0f4e819b295aed96f174ed96429
Pepe Valdivia <jvaldivia@softweb.pe>
2018-07-04 21:07:32 -0500

    Moviendo archivos de configuración a src/configs

--- src/configs/settings.php
+++ src/configs/settings.php
@@ -6,13 +6,13 @@ return [
 
         // Renderer settings
         'renderer' => [
-            'template_path' => __DIR__ . '/../templates/',
+            'template_path' => __DIR__ . '/../../templates/',
         ],
 
         // Monolog settings
         'logger' => [
             'name' => 'slim-app',
-            'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+            'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
     ],
