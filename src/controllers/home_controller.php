1e25786691bb74b7d2e50625e9be30ab8a75abb4
Pepe Valdivia <jvaldivia@softweb.pe>
2018-07-06 12:42:20 -0500

    Usando home con otro partial

--- src/controllers/home_controller.php
+++ src/controllers/home_controller.php
@@ -3,11 +3,22 @@
 class HomeController extends Controller
 {
   public function view($request, $response, $args) {
+    $this->load_helper('home');
     $rpta = '';
     $status = 200;
     $locals = [
       'constants' => $this->constants,
-      'title' => 'Ubicaciones',
+      'title' => 'Login',
+      'csss' => $this->load_css(index_css($this->constants)),
+      'jss'=> $this->load_js(index_js($this->constants)),
+      'mensaje' => '',
+      'menu' => '[{"url" : "accesos", "nombre" : "Accesos"},{"url" : "libros", "nombre" : "Libros"}]',
+      'items' => '[{"subtitulo":"","items":[{"item":"Gestión de Sistemas","url":"accesos/#/sistema"},{"item":"Gestión de Usuarios","url":"accesos/#/usuario"}]}]',
+      'data' => json_encode(array(
+        'mensaje' => false,
+        'titulo_pagina' => 'Gestión de Accesos',
+        'modulo' => 'Accesos'
+      )),
     ];
     $view = $this->container->view;
     return $view($response, 'app', 'home/index.phtml', $locals);
--- src/helpers/home_helper.php
+++ src/helpers/home_helper.php
@@ -8,15 +8,17 @@ if ( ! function_exists('index_css'))
       case 'desarrollo':
         $rpta = [
           'bower_components/bootstrap/dist/css/bootstrap.min',
-          'bower_components/font-awesome/css/font-awesome.min',
+				  'bower_components/font-awesome/css/font-awesome.min',
           'bower_components/swp-backbone/assets/css/constants',
-          'bower_components/swp-backbone/assets/css/login',
-				  //'css/style'
+          'bower_components/swp-backbone/assets/css/dashboard',
+          'bower_components/swp-backbone/assets/css/table',
+          'assets/css/constants',
+          'assets/css/styles',
         ];
         break;
       case 'produccion':
         $rpta = [
-          'dist/login.min',
+          'dist/ubicaciones.min',
         ];
         break;
     }
@@ -31,10 +33,31 @@ if ( ! function_exists('index_js'))
     switch($constants['ambiente_static']){
       case 'desarrollo':
         $rpta = [
+          'bower_components/jquery/dist/jquery.min',
+          'bower_components/bootstrap/dist/js/bootstrap.min',
+          'bower_components/underscore/underscore-min',
+          'bower_components/backbone/backbone-min',
+          'bower_components/backbone.marionette/lib/backbone.marionette.min',
+          'bower_components/handlebars/handlebars.min',
+          'bower_components/swp-backbone/layouts/application',
+          'bower_components/swp-backbone/views/table',
+          'bower_components/swp-backbone/views/modal',
+          'models/ubicaciones/departamento',
+          'models/ubicaciones/provincia',
+          'collections/ubicaciones/departamento_collection',
+          'collections/ubicaciones/provincia_collection',
+          'data/ubicaciones/tabla_distrito',
+          'data/ubicaciones/tabla_provincia',
+          'data/ubicaciones/tabla_departamento',
+          'views/ubicaciones/tabla_provincia_view',
+          'views/ubicaciones/tabla_departamento_view',
+          'views/ubicaciones/ubicacion_view',
+          'routes/ubicaciones',
         ];
         break;
       case 'produccion':
         $rpta = [
+          'dist/ubicaciones.min',
         ];
         break;
     }
