283c3ff89f0cf28854a2e2e75f14eb3409fa45ca
Pepe Valdivia <jvaldivia@softweb.pe>
2021-06-23 21:38:40 -0500

    avance de home

--- application/controllers/home_controller.php
+++ application/controllers/home_controller.php
@@ -10,17 +10,9 @@ class HomeController extends \Configs\Controller
     $status = 200;
     $locals = [
       'constants' => $this->constants,
-      'title' => 'Login',
+      'title' => 'Admin',
       'csss' => $this->load_css(index_css($this->constants)),
       'jss'=> $this->load_js(index_js($this->constants)),
-      'mensaje' => '',
-      'menu' => '[{"url" : "", "nombre" : "Ubicaciones"}]',
-      'items' => '[{"subtitulo":"","items":[{"item":"Ubicaciones del Perú","url":"#/ubicacion"},{"item":"Autocompletar","url":"#/autocompletar"}]}]',
-      'data' => json_encode(array(
-        'mensaje' => false,
-        'titulo_pagina' => 'Gestión de Ubicaciones',
-        'modulo' => 'Ubicaciones'
-      )),
     ];
     $view = $this->container->view;
     return $view($response, 'app', 'home/index.phtml', $locals);
--- application/helpers/home_helper.php
+++ application/helpers/home_helper.php
@@ -8,8 +8,12 @@ if ( ! function_exists('index_css'))
       case 'desarrollo':
         $rpta = [
           'bower_components/bootstrap/dist/css/bootstrap.min',
+          'bower_components/font-awesome/css/font-awesome.min',
           'assets/css/constants',
-          'assets/css/login',
+          'assets/css/table',
+          'assets/css/autocomplete',
+          'assets/css/styles',
+          'assets/css/admin',
         ];
         break;
       case 'produccion':
